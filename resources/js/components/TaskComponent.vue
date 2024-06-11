a096b0a38fc49eaa9dc742661b4df38cdb75c4d6
asimaahmood <aabuashar@gmail.com>
2024-06-11 15:52:58 +0500

    config of project

---
+++ resources/js/components/TaskComponent.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">SheMeansWork Task Component</div>
+                <div class="card-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputNumber" class="form-label">Enter Number</label>
+                            <input type="number" class="form-control" id="exampleInputNumber" aria-describedby="number">
+                            <div class="form-text">Enter Numeric Data.</div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
